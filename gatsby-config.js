ab805812e23c89474eced6c1f60fc5031fef63cb
Stathis Souris <ssouris@users.noreply.github.com>
2021-01-02 16:27:29 +0000

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,6 @@ module.exports = {
       options: {
         configPath: `src/config`,
         docsPath: `src/docs`,
-        githubUrl: `https://github.com/rocketseat/gatsby-themes`,
         baseDir: `examples/gatsby-theme-docs`,
       },
     },
